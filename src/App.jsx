eda56a1cb98028a85ce2d6ecbe71a5defa98bd26
www-NurzhiGit <nurjigit.zwer@gmail.com>
2024-06-29 02:10:59 +0600

    third commit

---
+++ src/App.jsx
@@ -0,0 +1,254 @@
+// import { useEffect, useState } from "react";
+
+// const _baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+// const apiKey = "f631ea87daddf959f8d7a12c30009e4c";
+// const App = () => {
+//   const [weatherData, setWeatherData] = useState(null);
+//   const [searchCity, setSearchCity] = useState("");
+//   async function getWeather(cityName = "Bishkek") {
+//     try {
+//       const res = await fetch(_baseUrl + cityName + "&appid=" + apiKey);
+//       const data = await res.json();
+//       console.log(data);
+//       setWeatherData(data);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+//   useEffect(() => {
+//     getWeather(searchCity);
+//   }, []);
+//   if (weatherData === null) {
+//     return <h1>Loading...</h1>;
+//   }
+//   const changeSearchCity = (e) => {
+//     setSearchCity(e.target.value);
+//   };
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="Search city"
+//         value={searchCity}
+//         onChange={changeSearchCity}
+//       />
+//       <button onClick={getWeather} ck>
+//         Search
+//       </button>
+//       <h2>{weatherData?.name}</h2>
+//       <h4>{Math.floor(weatherData.main.temp - 273.15)}°C</h4>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import { useEffect, useState } from "react";
+// import "./App.css";
+
+// const _baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+// const apiKey = "f631ea87daddf959f8d7a12c30009e4c";
+// const App = () => {
+//   const [weatherData, setWeatherData] = useState(null);
+//   const [searchCity, setSearchCity] = useState("");
+
+//   const [theme, setTheme] = useState("day");
+
+//   async function getWeather(cityName) {
+//     if (!cityName) return;
+//     try {
+//       const res = await fetch(_baseUrl + cityName + "&appid=" + apiKey);
+//       const data = await res.json();
+//       console.log(data);
+//       setWeatherData(data);
+//       // updateTheme(data);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+//   useEffect(() => {
+//     getWeather(searchCity);
+//   }, []);
+
+//   const updateTheme = (data) => {
+//     const localTime = new Date(
+//       (data.dt + data.timezone - new Date().getTimezoneOffset() * 60) * 1000
+//     );
+//     const hours = localTime.getUTCHours();
+//     if (hours >= 6 && hours < 18) {
+//       setTheme("day");
+//     } else {
+//       setTheme("night");
+//     }
+//   };
+
+//   const changeSearchCity = (e) => {
+//     setSearchCity(e.target.value);
+//   };
+
+//   const handleSearch = () => {
+//     getWeather(searchCity);
+//   };
+
+//   if (weatherData === null) {
+//     return <h1>Loading...</h1>;
+//   }
+//   // const btnGetWeather = () => {
+//   //   getWeather(searchCity);
+//   // };
+//   return (
+//     <div className={theme}>
+//       <input
+//         type="text"
+//         placeholder="Search city"
+//         value={searchCity}
+//         onChange={changeSearchCity}
+//       />
+
+//       <button onClick={() => getWeather(searchCity)}>Search</button>
+//       <h2>{weatherData?.name}</h2>
+//       <h4>{Math.floor(weatherData.main.temp - 273.15)}°C</h4>
+
+//       <div className="weather-details">
+//         <p>Cloudy: {weatherData.clouds.all}%</p>
+//         <p>Humidity: {weatherData.main.humidity}%</p>
+//         <p>Wind: {weatherData.wind.speed} km/h</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import { useEffect, useState } from "react";
+import "./App.css";
+import { CiSearch } from "react-icons/ci";
+
+const _baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "f631ea87daddf959f8d7a12c30009e4c";
+const defaultCity = "Bishkek";
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [searchCity, setSearchCity] = useState(defaultCity);
+  // тема HTTPS
+  const [theme, setTheme] = useState("day");
+
+  async function getWeather(cityName) {
+    if (!cityName) return;
+    try {
+      const res = await fetch(_baseUrl + cityName + "&appid=" + apiKey);
+      if (!res.ok) {
+        throw new Error("Не удалось получить данные о погоде");
+      }
+      const data = await res.json();
+      setWeatherData(data);
+      updateTheme(data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getWeather(searchCity);
+  }, [searchCity]);
+
+  const updateTheme = (data) => {
+    // Местное время
+    const localTime = new Date(
+      (data.dt + data.timezone - new Date().getTimezoneOffset() * 60) * 1000
+    );
+    const hours = localTime.getUTCHours();
+    if (hours >= 5 && hours < 18) {
+      setTheme("night");
+    } else {
+      setTheme("day");
+    }
+  };
+
+  const changeSearchCity = (e) => {
+    setSearchCity(e.target.value);
+  };
+
+  const handleSearch = () => {
+    getWeather(searchCity);
+  };
+
+  if (!weatherData) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div className={`app ${theme}`}>
+      <div className="weather-card">
+        <div className="weather-main">
+          <section className="sector-one">
+            <h2 className="weather-temp">
+              {Math.floor(weatherData.main.temp - 273.15)}°c
+            </h2>
+          </section>
+          <section>
+            <h2 className="weather-city">{weatherData.name}</h2>
+            <div>
+              <h4 className="weather-date">
+                {new Date(
+                  (weatherData.dt + weatherData.timezone) * 1000
+                  // Локальная строка
+                ).toLocaleString("en-GB", {
+                  hour: "2-digit",
+                  weekday: "long",
+                  minute: "2-digit",
+                  timeZone: "UTC",
+                })}
+              </h4>
+              <h4 className="calendarData">
+                {new Date(
+                  (weatherData.dt + weatherData.timezone) * 1000
+                  // Локальная строка
+                ).toLocaleString("en-GB", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </h4>
+              <h4 className="weather-desc">
+                {weatherData.weather[0].description}
+              </h4>
+            </div>
+          </section>
+        </div>
+      </div>
+      <div className="search-bar">
+        <input
+          type="text"
+          // ищите в другом месте
+          placeholder="search elsewhere"
+          value={searchCity}
+          onChange={changeSearchCity}
+        />
+        <button onClick={handleSearch}>
+          <CiSearch />
+        </button>
+        <hr className="hhr" />
+        <div className="location-list">
+          <ul>
+            <li>Birmingham</li>
+            <li>Manchester</li>
+            <li>New York</li>
+            <li>California</li>
+          </ul>
+        </div>
+        <hr />
+        <div className="weather-details">
+          <h3>weather-details</h3>
+          <p>Cloudy: {weatherData.clouds.all}%</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Wind: {weatherData.wind.speed} km/h</p>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default App;
